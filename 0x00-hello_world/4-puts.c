77eca978c3985b3c96b59a9f34a0bf466067187f
Ayinla Abel <kraneabel@gmail.com>
2022-04-07 17:00:00 -0700

    4-puts.c Added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - prints the phrase "Programming is like building a multilingual puzzle
  * Return: 0 if exited properly, non-zero otherwise
  */
-int main(void) 
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
