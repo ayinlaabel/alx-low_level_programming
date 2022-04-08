85eb8ee6b410e7f34ff36342dc83af1d3b3e9695
Ayinla Abel <kraneabel@gmail.com>
2022-04-08 07:07:34 -0700

    1-last_digit.c Added

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,6 +7,10 @@
 
 
 /* betty style doc for function main goes there */
+/**
+ * main -  function main
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -20,7 +24,7 @@ lastDigit = n % 10;
 
 if (lastDigit > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n,lastDigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else
 {
