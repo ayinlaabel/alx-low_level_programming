4ee26231ef8c8e1136d9c1a6ee85aad69711533e
Ayinla Abel <kraneabel@gmail.com>
2022-04-08 09:33:07 -0700

    9-print_comb.c Added

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return:0
+ */
+
+int main(void)
+{
+int c = 0;
+
+while (c < 10)
+{
+putchar(48 + c);
+if (c != 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+c++
+}
+putchar('\n');
+return (0);
+}
