fd54a080e9c700679c8544d274b57f5ba2f63f50
Ayinla Abel <kraneabel@gmail.com>
2022-04-08 08:19:29 -0700

    4-print_alphabt.c Added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * return:0
+ */
+
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+if(ch =='e' || ch == 'q')
+{
+}
+else
+{
+putchar(ch);
+putchar('\n');
+}
+return (0);
+}
