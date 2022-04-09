d9f511427575507994b7c2e5d6d7e7a9f4b2d90b
Ayinla Abel <kraneabel@gmail.com>
2022-04-08 17:42:26 -0700

    101-print_comb4.c Added

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+	int c = 0;
+
+	while (c < 10)
+	{
+		b = 0;
+
+		while (b < 10)
+		{
+			a = 0;
+			while (a < 10)
+			{
+				if (a != b && b != c && c != b && b != a)
+				{
+					putchar('0' + c);
+					putchar('0' + b);
+					putchar('0' + a);
+
+					if (a + b + c != 9 + 8 + 7)
+					{
+
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				a++;
+			}
+			b++;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
