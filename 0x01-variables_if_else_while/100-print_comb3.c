274819f09265c56375f169b41d7932079d05e1e1
Ayinla Abel <kraneabel@gmail.com>
2022-04-08 17:28:29 -0700

    100-print_comb3.c Added

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: 0
+ */
+
+int main(void)
+{
+int a;
+int b = 0;
+while (b < 10)
+{
+c = 0;
+while (c < 10)
+{
+if (d != c && d < c)
+{
+putchar('0' + d);
+putchar('0' + c);
+if (c + d != 17)
+{
+putchar(',');
+putchar(' ');
+}
+}
+c++;
+}
+d++
+}
+putchar('\n');
+return (0);
+}
