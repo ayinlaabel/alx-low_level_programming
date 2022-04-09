d8aa2e8b629d6c67314c58ee7b0f5a576e61194d
Ayinla Abel <kraneabel@gmail.com>
2022-04-08 18:12:14 -0700

    102-print_comb5.c Added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	int b;
+	int c;
+
+	int d, e, f;
+
+	while (a <= 98)
+	{
+		b = (a / 10 + '0');
+		c = (a % 10 + '0');
+
+		while (d < 99)
+		{
+			e = (d / 10 + '0');
+			f = (d % 10 + '0');
+
+			if (a < d)
+			{
+				putchar(b);
+				putchar(c);
+				putchar(' ');
+				putchar(e);
+				putchar(f);
+
+				if ( a != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			d++;
+		}
+		a++
+	}
+	putchar('\n');
+	return (0);
+}
