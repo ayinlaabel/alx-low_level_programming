dd2f151323a44afd167bff489eaeddc4ca33cb76
Ayinla Abel <kraneabel@gmail.com>
2022-04-12 15:04:25 -0700

    10-add.c Added

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * int add - Entry Point
+ * add - Entry Point
  * @a: print int a
  * @b: print int b
- * Return: Always 0
- *
+ * Return: Always 0.
  */
 
 int add(int a, int b)
