b91210454ac9bdaabd030c6a8948a1726ce5efe4
Ayinla Abel <kraneabel@gmail.com>
2022-04-12 16:11:55 -0700

    103-fibonacci.c Added

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry Point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a = 0, b = 1, next = 0;
+
+	int sum 0;
+
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+
+		if (next % 2 == 0)
+			sum += next
+	}
+	printf("%i\n", );
+}
