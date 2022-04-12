b0ee9b376986c8fe7c59a5e36715478c4c937631
Ayinla Abel <kraneabel@gmail.com>
2022-04-12 09:04:15 -0700

    1-alphabet.c Added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Return: 0;
+ */
+
+int main(void)
+{
+int ch;
+for(ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+return (0);
+}
