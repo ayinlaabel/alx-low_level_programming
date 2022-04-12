d17981cd4b14fa72d8d5d61c5d1051e55125e96e
Ayinla Abel <kraneabel@gmail.com>
2022-04-12 15:52:59 -0700

    101.natural.c Added

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 
 /**
- * main - Entry Poin
+ * main - Entry Point
  * Return: 0
  */
 
 int main(void)
 {
 	int c = 0, sum = 0;
-	
 	while (c < 1024)
 	{
 		if (c % 3 == 0 || c % 5 == 0)
