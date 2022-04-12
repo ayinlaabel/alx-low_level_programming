56117bd741373954cf2be437091d52f0c472f12b
Ayinla Abel <kraneabel@gmail.com>
2022-04-12 14:10:07 -0700

    8-24_hours Added

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minutes of the day
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 23; a++)
+	{
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar(a / 10 + '0');
+			_putchar(a % 10 + '0');
+			_putchar(":");
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
