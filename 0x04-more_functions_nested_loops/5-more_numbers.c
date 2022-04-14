90d4313706f0140f45cb63350a66afa840c8e854
Ayinla Abel <kraneabel@gmail.com>
2022-04-14 06:50:18 -0700

    5-more_numbers.c Added

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,11 @@ void more_numbers(void)
 	{
 		for (a = 0; a <= 14; a++)
 		{
-			_putchar('0' + a);
+			if ( a > 9)
+			{
+				_putchar(a / 10 + '0');
+			}
+			_putchar(a % 10 + '0');
 		}
 
 		_putchar('\n');
