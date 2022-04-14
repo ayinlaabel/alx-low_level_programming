99015130fe595295dba1d9a5ca02d6baf8163b76
Ayinla Abel <kraneabel@gmail.com>
2022-04-14 06:37:53 -0700

    5-more_numbers.c Added

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ * Return: always 0
+ */
+
+void more_numbers(void)
+{
+	int counter = 0;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			_putchar('0' + a);
+		}
+
+		_putchar('\n');
+	}
+}
