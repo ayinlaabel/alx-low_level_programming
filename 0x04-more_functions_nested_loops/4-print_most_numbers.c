e1b9d3507eece7b882740a870c0d3665ad1e4443
Ayinla Abel <kraneabel@gmail.com>
2022-04-14 06:11:34 -0700

    4-print_most_numbers.c Added

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ * Return: always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
