af952bd95d88fb8c051a9e6f2dfeb5de07c1c0c4
Ayinla Abel <kraneabel@gmail.com>
2022-04-14 05:30:54 -0700

    2-mul.c Added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: varaible 1
+ * @b: varaible 2
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+	printf("%d\n", c);
+	return (0);
+}
