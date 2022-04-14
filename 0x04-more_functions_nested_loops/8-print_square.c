90c3a0592de54e0fc6432355bf8928fb4e17abf2
Ayinla Abel <kraneabel@gmail.com>
2022-04-14 07:29:20 -0700

    8-print_square.c Added

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - entry point
+ * @n: number
+ * Return: 0
+ */
+
+void print_square(int n)
+{
+	int i = 0, a;
+
+	while (i < n && n > 0)
+	{
+		a = 0;
+		while (a < n )
+		{
+			_putchar('#');
+			a++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+}
