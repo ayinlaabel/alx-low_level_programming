8ab2c51f6460512a835e7d51676114d34c21b5b0
Ayinla Abel <kraneabel@gmail.com>
2022-04-14 05:44:22 -0700

    3-print_numbers.c Added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry Point
+ * Return: always 0
+ */
+
+void print_numbers(void)
+{
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
