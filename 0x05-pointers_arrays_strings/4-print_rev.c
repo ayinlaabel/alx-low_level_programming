0081c50e9fc20f3c04edf36689db682457b219bc
Ayinla Abel <kraneabel@gmail.com>
2022-04-20 07:04:20 -0700

    4-print_rev.c Added

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
+
 /**
  * print_rev -  Entry Point
  * @s: Varaible
@@ -14,9 +16,9 @@ void print_rev(char *s)
 
 	for (i = 0; i < len / 2; i++)
 	{
-		t = a[i];
-		a[i] = a[len - i - 1];
-		a[len - i - 1] = t;
+		t = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = t;
 	}
 	printf("%s\n", s);
 }
