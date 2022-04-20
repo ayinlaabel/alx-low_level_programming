a281d79811a7f5b2960400bb81b0339a238e1984
Ayinla Abel <kraneabel@gmail.com>
2022-04-20 09:27:04 -0700

    6-puts2.c Added

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts2 - Entry Point
+ * @str: Varaible
+ * Return: Always 0
+ */
+
+void puts2(char *str)
+{
+	int i, len;
+	
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+	}
+	putchar('\n');
+}
