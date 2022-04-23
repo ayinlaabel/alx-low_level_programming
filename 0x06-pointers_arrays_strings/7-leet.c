2ba0b82c6ee61f75907f609fa8b1afa9b37f6676
Ayinla Abel <kraneabel@gmail.com>
2022-04-23 08:51:54 -0700

    7-leet.c Added

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - Entry Point
+ * @str: Variable
+ * Return: Always 0
+ */
+
+char *leet(char *str)
+{
+	int i, x;
+	char a[] = "aeotl";
+	char b[] = "AEOTL";
+	char c[] = "43071";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (x = 0; x < 5; x++)
+		{
+			if (str[i] == a[x] || str[i] == b[x])
+			{
+				str[i] = c[x];
+				break;
+			}
+		}
+	}
+	return (str);
+}
