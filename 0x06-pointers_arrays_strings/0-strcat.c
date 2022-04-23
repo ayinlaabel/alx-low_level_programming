388b23edeeea4a77b024702354ed71e6bbe179d5
Ayinla Abel <kraneabel@gmail.com>
2022-04-23 06:53:27 -0700

    0-strcat.c Added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,19 +4,20 @@
  * *_strcat - Entry Point
  * @dest: Destination Variable
  * @src: Source Variable
+ * Return: Always 0
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int a= -1, i;
-	for (i = 0; dest[i] != '\0'; i++)
-	;
+int a= -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
 
-	do {
-		a++;
-		dest[i] = src[a];
-		i++;
-	} while (src[a] != '\0');
+do {
+	a++;
+	dest[i] = src[a];
+	i++;
+} while (src[a] != '\0');
 
-	return (dest);
+return (dest);
 }
