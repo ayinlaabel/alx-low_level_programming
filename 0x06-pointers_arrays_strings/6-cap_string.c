581fbfdf542e4ee3618f01cb58904c458daebac6
Ayinla Abel <kraneabel@gmail.com>
2022-04-23 08:25:21 -0700

    6-cap_string.c Added

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * *cap_string - Entry Point
+ * @str: Variable
+ * Return: Always 0
+ */
+
+char *cap_string(char *str)
+{
+	char x[] = ",\t;\n; .!?\"(){}";
+	int a, b, c;
+
+	for (i = 0; str[b] != '\0'; i++)
+	{
+		a = 0;
+
+		if (i == 0)
+		{
+			a = 1;
+		}
+		else
+		{
+			for (c = 0; x[c] != '\0'; c++)
+			{
+				if (str[b - 1] == x[c])
+				{
+					a = 1;
+					break;
+				}
+			}
+		}
+
+		if (a == 1)
+		{
+			if (str[b] <= 'z' && str[i] >= 'a')
+			{
+				str[b] -= ('a' - 'A');
+			}
+		}
+	}
+	return (str);
+}
