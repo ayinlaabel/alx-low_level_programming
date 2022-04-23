8a255fca0a67fac693d56dae7d2d0b81e985eb4d
Ayinla Abel <kraneabel@gmail.com>
2022-04-23 07:45:16 -0700

    3-strcmp.c Added

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Entry Point
+ * @s1: First Variable
+ * @s2: Second Variable
+ * Return: Always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
