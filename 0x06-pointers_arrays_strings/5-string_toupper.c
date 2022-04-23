79272f9c58c9c5fcf2c441ca3cb3e364423753cb
Ayinla Abel <kraneabel@gmail.com>
2022-04-23 08:12:04 -0700

    5-string_toupper.c Added

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *string_toupper - Entry Point
+ * @str: Variable
+ * Return: Always 0
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] <= 'z' && str[i] >= 'a')
+			str[i] -= 32;
+	}
+	return (str);
+}
