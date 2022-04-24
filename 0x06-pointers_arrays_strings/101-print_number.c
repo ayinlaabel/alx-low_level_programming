6737539b532fc77c80a2f205830b55ea6941b937
Ayinla Abel <kraneabel@gmail.com>
2022-04-24 12:11:09 -0700

    101-print_number.c Added

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - Entry Point
- * @n: variable
+ * print_number - print numbers chars
+ * @n: int
  * Return: Always 0
  */
 
 void print_number(int n)
 {
-	unsigned int a;
+	unsigned int n1;
 
-	a = n;
+	n1 = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		a = -n;
+		n1 = -n;
 	}
 
-	if (a / 10 != 0)
+	if (n1 / 10 != 0)
 	{
-		print_number(a / 10);
+		print_number(n1 / 10);
 	}
-	_putchar((a % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
