3266e0a9661ed111219becc571b21a947335ea60
Ayinla Abel <kraneabel@gmail.com>
2022-04-24 17:02:24 -0700

    104-print_buffer.c Added

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,9 +11,9 @@
 void print_buffer(char *b, int size)
 {
 	int o, j, i;
-	
+
 	o = 0;
-	
+
 	if (size <= 0)
 	{
 		printf("\n");
@@ -37,7 +37,7 @@ void print_buffer(char *b, int size)
 		for (i = 0; i < j; i++)
 		{
 			int c = *(b + o + i);
-			
+
 			if (c < 32 || c > 132)
 			{
 				c = '.';
