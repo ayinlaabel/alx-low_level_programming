9df295adc8817d9797db51503358ed0c9aea12d8
Ayinla Abel <kraneabel@gmail.com>
2022-04-26 05:32:19 -0700

    0-memset.c Added

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,14 +5,14 @@
  * @s: Variable
  * @b: Variable
  * @n: Loop
- * Return: always 0
+ * Return: Always 0
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
