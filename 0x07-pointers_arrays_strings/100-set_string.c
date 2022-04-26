9277c6dc41e988303bd76406331d4c44c7db0a14
Ayinla Abel <kraneabel@gmail.com>
2022-04-26 09:38:50 -0700

    100-set_string.c Added

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - Entry Point
+ * @s: String Variable
+ * @to: Value Variable
+ * Return: Always 0
+ */
+
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
