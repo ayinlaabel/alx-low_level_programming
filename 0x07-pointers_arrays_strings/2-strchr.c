c34c8f0cdc7e5c6b21f9fd961df9aa6be887ea97
Ayinla Abel <kraneabel@gmail.com>
2022-04-26 05:43:01 -0700

    2-strchr.c Added

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry Point
+ * @s: String Variable
+ * @c: Character Variable
+ * Return: Always 0
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+	return (0);
+}
