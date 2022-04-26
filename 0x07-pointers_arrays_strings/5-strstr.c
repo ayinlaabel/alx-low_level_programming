0c5b3f02e128c15589e37255b48bf2e806dec4fb
Ayinla Abel <kraneabel@gmail.com>
2022-04-26 06:08:41 -0700

    5-strstr.c Added

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry Point
+ * @haystack: Variable
+ * @needle: Variable
+ * Return: Always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, a;
+
+	if (needle[0] == '\0')
+	{
+		return (haystack);
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if ( haystack[i] == needle[0])
+		{
+			for (a = 0; needle[a] != '\0'; a++)
+			{
+				if (haystack[i + a] != needle[a])
+				{
+					break;
+				}
+			}
+			if (needle[a] == '\0')
+			{
+				return (haystack + 1);
+			}
+		}
+	}
+		return ('\0');
+}
+	
