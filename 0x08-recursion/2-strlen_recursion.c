3df9769f16dd77a96cc9115f2787fe97c07b5912
Ayinla Abel <kraneabel@gmail.com>
2022-04-28 10:01:41 -0700

    2-strlen_recursion.c Added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Entry Point
+ * @s: Variable
+ * Return: Always 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s);
+}
