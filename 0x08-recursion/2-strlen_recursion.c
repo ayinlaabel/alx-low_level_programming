35b9eb95867e9bc2e182a03580bae27801ec409f
Ayinla Abel <kraneabel@gmail.com>
2022-04-28 10:13:36 -0700

    2-strlen_recursion.c Added

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,9 +8,12 @@
 
 int _strlen_recursion(char *s)
 {
-	if (!s)
+	int n = 0;
+
+	if (*s)
 	{
-		return (0);
+		n = _strlen_recursion(s + 1);
+		return (n += 1);
 	}
-	return (1 + _strlen_recursion(s + 1));
+	return (0);
 }
