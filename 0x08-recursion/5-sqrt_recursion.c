06123679a3feead94b833c403ad48b8b9fb3afed
Ayinla Abel <kraneabel@gmail.com>
2022-04-28 10:31:05 -0700

    5-sqrt_recursion.c Added

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Entry Point
+ * @n: Variable
+ * Return: Always 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - _sqrt_rescursion (Entry Point)
+ * @n: Variable
+ * @i: Variable
+ * Return: _sqrt
+ */
+
+int _sqrt(int n, int i)
+{
+	if (n< 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if ((i * i) == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
