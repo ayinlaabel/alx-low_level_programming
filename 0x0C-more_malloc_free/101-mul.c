a7b9b53baef5bfab2e616e2539b0c7fad62423e3
Ayinla Abel <kraneabel@gmail.com>
2022-05-09 09:00:24 -0700

    101-mul.c Added

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 
 /**
  * main - multiplies two positive numbers
@@ -12,29 +9,23 @@
 
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int num1, num2;
+unsigned long mul;
+int i, j;
 
 	if (argc != 3)
+	{ printf("Error\n");
+		exit(98); }
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error \n");
-		exit(98);
-	}
-
-	for (num1 = 1; num1 < argc; num1++)
-	{
-		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
-			{
-				printf("Error \n");
-				exit(98);
-			}
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+				exit(98); }
 		}
+
 	}
-
-	mul = atol(argv[1]) * atol(argv[2]);
+	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
-
-	return (0);
+return (0);
 }
