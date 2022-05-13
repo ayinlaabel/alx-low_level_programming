b32fc30b00717a0872529648ca8ed57aa49c2d9a
Ayinla Abel <kraneabel@gmail.com>
2022-05-13 06:28:21 -0700

    3-calc.h Added

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,26 @@
+#ifndef _CAALC_H_
+#define _CAALC_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct op - Struct op
+ * @op: The Operator
+ * @f: The function associated
+ */
+
+typedef struct op
+{
+	char *op;
+	int (8f)(int a, int b);
+} op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif
