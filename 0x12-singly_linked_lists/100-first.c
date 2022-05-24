68eba50a25c930ef2b5e6045cf39d9246fd9e0b3
Ayinla Abel <kraneabel@gmail.com>
2022-05-24 09:05:43 -0700

    100-first.c Added

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * bmain - function executed before main
+ * Return: always 0
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
+}
