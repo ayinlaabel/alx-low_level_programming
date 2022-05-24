6b230a255d676842397948e602694af0444dbd2d
Ayinla Abel <kraneabel@gmail.com>
2022-05-24 08:30:29 -0700

    0-print_list.c Updated

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,10 +11,10 @@ size_t print_list(const list_t *h)
 	size_t nelem;
 
 	nelem = 0;
-	while(h != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf ("[%d] %s\n", 0, "(nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
