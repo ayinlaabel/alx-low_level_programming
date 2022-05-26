e4bd2aca9bf7370e452229c3594170e828d46979
Ayinla Abel <kraneabel@gmail.com>
2022-05-26 08:33:24 -0700

    lists.h updated

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,37 @@
 #ifndef _LISTS_
 #define _LISTS_
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: int
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: point to the next node
+ *
+ * Description: singly linked list of pointer
+ */
+
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
 size_t print_listint(const listint_t *h);
 
 #endif
