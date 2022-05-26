4dda4a0f483dc970c6feac5b9194fd896cf6cfcb
Ayinla Abel <kraneabel@gmail.com>
2022-05-26 09:55:09 -0700

    9-insert)nodeint.c Added

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t *head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (i = 0; i < idx -1 && h != NUL:L; i++)
+		for (i = 0; i < idx - 1 && h != NULL; i++)
 		{
 			h = h->next;
 		}
