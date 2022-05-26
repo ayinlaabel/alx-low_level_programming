28bf8551e1e9d599eb34e15ebdb3b45a924a1d8f
Ayinla Abel <kraneabel@gmail.com>
2022-05-26 09:16:10 -0700

    4-free_listint.c Added

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: head of a list
+ * Return: always 0
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
+}
