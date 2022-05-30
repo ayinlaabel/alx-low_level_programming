ce4a987ec4f47a4cb751fc5871378be9527911c3
Ayinla Abel <kraneabel@gmail.com>
2022-05-30 09:27:03 -0700

    5-flip_bits.c updated

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int i;
 
-	for ( i = 0; n || m; n >>= 1, m >>= 1)
+	for (i = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			i++;
