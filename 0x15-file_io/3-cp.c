c882e69e4b57c72f73acb6d519ea6da4c7d1e6f4
Ayinla Abel <kraneabel@gmail.com>
2022-06-01 09:11:49 -0700

    3-cp.c Added

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -54,11 +54,11 @@ int main(int argc, char *argv[])
 			error_file(-1, 0, argv);
 
 		nwr = write(file_to, buf, nchars);
-		if(nwr == -1)
+		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
 	err_close = close(file_from);
-	if(err_close == -1)
+	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
