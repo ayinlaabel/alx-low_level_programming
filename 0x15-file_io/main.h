1263e2f6b81aa42eb25998b27a694f52fc2294d6
Ayinla Abel <kraneabel@gmail.com>
2022-06-01 08:32:09 -0700

    main.h updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,12 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
