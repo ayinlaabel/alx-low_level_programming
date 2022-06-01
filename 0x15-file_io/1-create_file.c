b63a91bd874b94304408969d370183f600cd4862
Ayinla Abel <kraneabel@gmail.com>
2022-06-01 08:42:32 -0700

    1-create_file.c updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
  * create_file - creates a file
- * #filename: filename
+ * @filename: filename
  * @text_content: content writed in the file
- * Return 1 if success then -1 if fails
+ * Return: 1 if success then -1 if fails
  */
 
 int create_file(const char *filename, char *text_content)
@@ -15,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open (filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
